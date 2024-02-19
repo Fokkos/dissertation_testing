846e2679bccb6716023ff4a0128449aff10da940
James March <54935498+Fokkos@users.noreply.github.com>
2024-02-19 23:57:59 +0000

    added support for different variable totals

--- flaskintro/app.py
+++ flaskintro/app.py
@@ -8,27 +8,37 @@ variables = 2
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    print(request.method)
+    global variables, candidates, voters  # Declare variables as global so they can be used inside index()
     if request.method == 'POST':
         form = request.form
-        if request.form['form_type'] == 'add_candidate':
-            point = extract_point()
+        
+        if form['form_type'] == 'add_candidate':
+            point = extract_point(form)
             candidates.append(point)
-        elif request.form['form_type'] == 'delete_candidate':
-            point = extract_point()
+        elif form['form_type'] == 'delete_candidate':
+            point = extract_point(form)
             candidates.remove(point)
-        if request.form['form_type'] == 'add_voter':
-            point = extract_point()
+        if form['form_type'] == 'add_voter':
+            point = extract_point(form)
             voters.append(point)
-        elif request.form['form_type'] == 'delete_voter':
-            point = extract_point()
+        elif form['form_type'] == 'delete_voter':
+            point = extract_point(form)
             voters.remove(point)
+        elif form['form_type'] == 'set_variables':
+            # TODO add confirmation for deleting all candidates and voters
+            variables = int(form['variables'])
+            candidates = []
+            voters = []
     return render_template('index.html', candidates=candidates, voters=voters, variables=variables)
 
-def extract_point():
-    x = int(request.form['x'])
-    y = int(request.form['y'])
-    return {'x': x, 'y': y}
+def extract_point(form):
+    points = {}
+    for i in range(variables):
+        # add point to temp dictionary
+        # TODO ideally have the ids be names by user
+        points[i] = (int(form['variable_' + str(i)]))
+    return points
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
