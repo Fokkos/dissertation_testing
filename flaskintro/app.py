48ddba0c94092b57d3a2cdb571022534b61c685b
Fokkos <jameswmarch2002@gmail.com>
2024-04-22 16:42:47 +0100

    setup code for minkowski and chebyshev

--- flaskintro/app.py
+++ flaskintro/app.py
@@ -165,7 +165,9 @@ def threejs():
                     default_candidates.append(candidate)
                     break
     
-    line_type = data.distance_measure
+    # by default, the lines generated are euclidean 
+    line_type = 'euclidean'
+    
     # colours source: https://sashamaps.net/docs/resources/20-colors/
     colours = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#ffffff', '#000000']
     return render_template('threejs.html', data=data, type='threejs', colours=colours, chosen_variables=chosen_variables, candidates=default_candidates, line_type=line_type)
--- flaskintro/calculations.py
+++ flaskintro/calculations.py
@@ -8,8 +8,7 @@ def choose_candidate(data, voter):
             distances.append((candidate['id'], euclidean_distance(voter, candidate, data.variables)))
         elif data.distance_measure == 'manhattan':
             distances.append((candidate['id'], manhattan_distance(voter, candidate, data.variables)))
-        # TODO: minkowski, discrete and hamming
-        # research mahalanobis, cosine, jaccard, pearson, spearman, kendall
+        # TODO: minkowski and chebyshev distance measures
     # sort the distances by shortest to longest
     voter['distances'] = sorted(distances, key=lambda x: x[1])
 
@@ -23,6 +22,20 @@ def manhattan_distance(voter, candidate, variables):
     distance = 0
     for i in range(variables):
         distance += abs(float(voter[i]) - float(candidate[i]))
+    return distance
+
+def minkowski_distance(voter, candidate, variables, p):
+    distance = 0
+    for i in range(variables):
+        distance += abs(float(voter[i]) - float(candidate[i])) ** p
+    return distance ** (1 / p)
+
+def chebyshev_distance(voter, candidate, variables):
+    distance = 0
+    for i in range(variables):
+        # for each variable, find the absolute difference between the voter and candidate
+        # and take the largest difference (including all previous distances)
+        distance = max(distance, abs(float(voter[i]) - float(candidate[i])))
     return distance
 
 def find_variances(data):
