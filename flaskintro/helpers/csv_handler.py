9d6e6c9e234e6ca730d8fce0bfcedc94ca7c33a3
Fokkos <jameswmarch2002@gmail.com>
2024-04-29 14:41:15 +0100

    fixed commenting error

--- flaskintro/helpers/csv_handler.py
+++ flaskintro/helpers/csv_handler.py
@@ -2,12 +2,12 @@ import pandas as pd
 import math  
 
 def process_csv(file, data):
-    df = pd.read_csv(file, encoding='unicode_escape')
-    # 1. warn user that this will overwrite existing data DONE
+    df = pd.read_csv(file, encoding='unicode_escape') # read the csv file using pandas
+    # 1. delete the previous data stored in the data object as new variable names and datapoints will be created
     data.delete_data()
     # 2. get the columns
     columns = df.columns
-    # assume first column is name
+    # assume first column is name/id of the candidate
     variable_names = columns[1:].values.tolist()
     # 3. update variable count to columns - 1 (account for name)
     data.variables = len(variable_names)
@@ -17,7 +17,7 @@ def process_csv(file, data):
     for i in range(len(df)):
         candidate = {}
         candidate['id'] = df.iloc[i, 0]
-        candidate['cost'] = 10
+        candidate['cost'] = 10 # cost is assumed to be 10 by default
         for j in range(len(variable_names)):
             candidate[j] = format(float(df.iloc[i, j + 1]), ".2f")
         data.candidates.append(candidate)
@@ -33,12 +33,3 @@ def process_csv(file, data):
     data.average_voter = data.update_average_voter()
     
     return df
-
-
-# df works!! now need to
-    # 1. warn user that this will overwrite existing data DONE
-    # 2. get the columns
-    # 3. update variable count to columns - 1 (account for name)
-    # 4. update variable names to column names
-    # 5. update candidates / voters with the data
-    # IDEA: have a SELECT to ask for voter or candidate data
